5c22dde84e519f6fc971609e496a182ba8361725
gloriaodipo <gloriaodipo@gmail.com>
2018-07-14 13:21:51 +0300

    [CHORE #159030328] Create endpoints for signup

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -1,15 +1,32 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 class UserSignupAPI(Resource):
     '''Resource for user registration'''
-    def post(self):
-        pass
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', required=True, help='Username cannot be blank', type=str)
+    parser.add_argument('email', required=True, help='Email cannot be blank', type=str)
+    parser.add_argument('password', required=True, help='Password cannot be blank')
 
-class UserLoginAPI(Resource):
-    '''Resource for user login'''
     def post(self):
-        pass
+        args = UserSignupAPI.parser.parse_args()
+
+        u = User(username=args.get('username'),
+            email=args.get('email'), password=args.get('password'))
+    
+        username_exists = db.get_user_by_username(args['username'])
+        email_exists = db.get_user_by_email(args['email'])
+
+        if username_exists:
+            return {'message': 'User already exists'}, 203
+        elif email_exists:
+            return {'message': 'User already exists'}, 203
+
+        db.add_user(u)
+
+        return {'message': 'Successfully registered'}, 201
+        
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+
 from app import create_app
 
 config = os.getenv('APP_SETTINGS')
