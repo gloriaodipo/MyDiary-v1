7bc6a055a9bce5c434f5427becf01cc761486de9
gloriaodipo <gloriaodipo@gmail.com>
2018-07-14 12:08:09 +0300

    [CHORE] Change file structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-import config
+from config import app_config
 
 def create_app(config_name):
     '''Method to create a flask app depending on the configuration passed'''
@@ -9,7 +9,7 @@ def create_app(config_name):
     app = Flask(__name__)
     api=Api(app)
 
-    app.config.from_object(config.app_config[config_name])
+    app.config.from_object(app_config[config_name])
     app.url_map.strict_slashes = False
 
     from app.resources.user_resource import SignupResource
