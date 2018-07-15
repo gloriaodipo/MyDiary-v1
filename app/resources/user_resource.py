e3557e94f71bff134fe046bb293d0830c8f93b43
gloriaodipo <gloriaodipo@gmail.com>
2018-07-15 22:48:45 +0300

    Update signup functions

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -1,36 +1,32 @@
 from flask import Flask
 from flask_restful import Resource, reqparse
-
-<<<<<<< HEAD
-app = Flask(__name__)
-api = Api(app)
-=======
+from werkzeug.security import generate_password_hash, check_password_hash
 from app.models import User
-from app.database import db
->>>>>>> e670316... Remove app from user resource
 
-class Signup_Resource(Resource):
+
+class SignupResource(Resource):
     '''Resource for user registration'''
     parser = reqparse.RequestParser()
     parser.add_argument('username', required=True, help='Username cannot be blank', type=str)
     parser.add_argument('email', required=True, help='Email cannot be blank', type=str)
-    parser.add_argument('password', required=True, help='Password cannot be blank')
+    parser.add_argument('password', required=True, help='Password cannot be blank', type=str)
 
     def post(self):
-        args = Signup_Resource.parser.parse_args()
+        args = SignupResource.parser.parse_args()
+        password = args.get('password')
+        username = args.get('username')
+        email = args.get('email')
+        if password.strip() == '' or username.strip() == '' or email.strip() == '':
+            return {'message': 'All fields are required.'}, 400
+        password_hash = generate_password_hash(password)
 
-        user = User(username=args.get('username'),
-            email=args.get('email'), password=args.get('password'))
-    
-        username_exists = db.get_user_by_username(args['username'])
-        email_exists = db.get_user_by_email(args['email'])
+        username_exists = User.get_user_by_username(username=args['username'])
+        email_exists = User.get_user_by_email(email=args['email'])
 
-        if username_exists:
+        if username_exists or email_exists:
             return {'message': 'User already exists'}, 203
-        elif email_exists:
-            return {'message': 'User already exists'}, 203
-
-        db.add_user(user)
-
-        return {'message': 'Successfully registered'}, 201
         
+        user = User(username=args.get('username'), email=args.get('email'), password=password_hash)
+        user = user.save()
+
+        return {'message': 'Successfully registered', 'user': user}, 201
