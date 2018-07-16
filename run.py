faa6a67a09403e46fb2ce58c2ef8fd659c04b98f
gloriaodipo <34505753+gloriaodipo@users.noreply.github.com>
2018-07-16 17:20:50 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ config = os.getenv('APP_SETTINGS')
 app = create_app(config)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
